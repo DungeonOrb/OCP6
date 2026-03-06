c088cb0336e33d035e8aadf8e4bb36174444130e
Ugo Leone <ugomael@gmail.com>
2026-03-06 16:47:28 +0100

    ajout de la page d'erreur 404

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,11 +1,23 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./NotFound.css";
 
 export default function NotFound() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>404</h1>
-      <p>Cette page n’existe pas.</p>
-      <Link to="/dashboard">Retour au dashboard</Link>
+    <div className="notfound">
+      <div className="notfound__card">
+        <h1 className="notfound__code">Erreur 404</h1>
+
+        <p className="notfound__text">Page non trouvée</p>
+
+        <button
+          className="notfound__btn"
+          onClick={() => navigate("/dashboard")}
+        >
+          Retour
+        </button>
+      </div>
     </div>
   );
 }
